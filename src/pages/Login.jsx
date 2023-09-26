8c8fa87ac2c3a5df8152816d287f71c22d5cf28c
anexdev1 <anwarnasir0970@gmail.com>
2023-09-26 08:02:52 -0900

    building the UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
+import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
-
+import { useState } from "react";
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
@@ -7,6 +8,7 @@ export default function Login() {
 
   return (
     <main className={styles.login}>
+      <PageNav />
       <form className={styles.form}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
@@ -29,7 +31,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <button className={styles.ctaLink}>Login</button>
         </div>
       </form>
     </main>
